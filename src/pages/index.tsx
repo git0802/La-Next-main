ad3875b41d45a6818ee350c865dcf4b885c045cd
tiggon112 <tiggon112@gmail.com>
2023-11-10 03:19:40 -0800

    index page email tweak

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,79 @@
-import Register from "./register";
 
-function Index() {
-  return <Register />
+import Image from 'next/image'
+import Link from 'next/link';
+
+export default function Index() {
+
+
+    return (
+        <>
+            <div className="bg-white">
+                <header>
+                    <nav className="flex justify-center pt-16" aria-label="Global">
+
+                        <Image src="learnanything.svg" alt="Learn Anything Logo" width={300} height={200} />
+
+                    </nav>
+
+                </header>
+
+                <div className="relative isolate">
+                    <div
+                        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                        aria-hidden="true"
+                    >
+                        <div
+                            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                            style={{
+                                clipPath:
+                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                            }}
+                        />
+                    </div>
+                    <div className=" my-24">
+                        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                            <div className="mx-auto max-w-2xl text-center">
+                                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                                    The world&apos;s first AI powered personal tutor</h1>
+                                <p className="mt-6 text-lg leading-8 text-gray-600">
+                                    Learn anything with conversational AI, human-like feedback, intelligently marked quizzes and much more. <b>Launching May 2024.</b>
+                                </p>
+                                <div className="mt-10 flex items-center justify-center gap-x-6">
+                                    <Link
+                                        href="mailto:info@learnanything.com"
+                                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Get in touch
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                        aria-hidden="true"
+                    >
+                        <div
+                            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                            style={{
+                                clipPath:
+                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+
+
+            <footer className="bg-white">
+                <div className="mx-auto max-w-7xl overflow-hidden px-6 lg:px-8">
+
+                    <p className="text-center text-xs leading-5 text-gray-500">
+                        &copy; 2023 Learn Anything All rights reserved.
+                    </p>
+                </div>
+            </footer>
+
+        </>
+    )
 }
-
-export default Index;
